3832d8a480b643ac2744a36adbed469b6eec3098
Kevin Wang <rocwang0316@gmail.com>
2019-05-03 23:19:40 -0700

    Added Acount model and its test code.

---
+++ __tests__/chaincode/Account.js
@@ -0,0 +1,223 @@
+import { ChaincodeMockStub, Transform } from '@theledger/fabric-mock-stub';
+import sinon from 'sinon';
+
+// import { mockDate, resetDateObject } from '../utils/MockDate';
+import Account from '../../src/chaincode/Account';
+// You always need your chaincode so it knows which chaincode to invoke on
+
+let mockStub;
+const DEPOSIT_TYPE = 'DEPOSIT';
+const WITHDRAW_TYPE = 'WITHDRAW';
+const INTEREST_TYPE = 'INTEREST';
+
+// Mock Date object
+let DATE_TO_USE;
+
+describe('Test Account Chaincode', () => {
+  beforeEach(() => {
+    mockStub = new ChaincodeMockStub('MyMockStub', new Account());
+    DATE_TO_USE = sinon.useFakeTimers({ now: 1517644800000 });
+  });
+
+  afterAll(() => {
+    DATE_TO_USE.restore();
+  });
+
+  test('Init without issues', async () => {
+    const response = await mockStub.mockInit('tx1', []);
+    expect(response.status).toBe(200);
+  });
+
+  test('Create and query an account', async () => {
+    let response = await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: '123', balance: 100, name: 'Kevin',
+    })]);
+
+    expect(response.status).toBe(200);
+
+    response = await mockStub.mockInvoke('tx1', ['queryAccount', JSON.stringify({ accountId: '123' })]);
+    expect(Transform.bufferToObject(response.payload)).toEqual({
+      accountId: '123',
+      balance: 100,
+      name: 'Kevin',
+      lastUpdate: new Date().toUTCString(),
+    });
+  });
+
+  // test.only('createTransaction', async () => {
+  //   let response = await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+  //     accountId: '123', balance: 100, name: 'Kevin',
+  //   })]);
+
+  //   expect(response.status).toBe(200);
+
+  //   response = await mockStub.mockInvoke('tx1', ['fetchTransactions', JSON.stringify({ accountId: '123' })]);
+  //   const { data } = Transform.bufferToObject(response.payload);
+  //   expect(data[1]).toEqual({
+  //     type: DEPOSIT_TYPE,
+  //     amount: 100,
+  //     name: 'Kevin',
+  //     date: DATE_TO_USE.toUTCString(),
+  //   });
+  // });
+
+  test('queryAccounts', async () => {
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'aa_123aaad3', balance: 200, name: 'KevinA',
+    })]);
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'aa_dsf123ef3', balance: 100, name: 'Kevin',
+    })]);
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'bb_dsf123ef3', balance: 100, name: 'Kevin',
+    })]);
+    const response = await mockStub.mockInvoke('tx1', ['queryAccounts', JSON.stringify({
+      startAccountId: 'aa_0',
+      endAccountId: 'aa_z',
+    })]);
+
+    expect(response.status).toBe(200);
+    const payload = Transform.bufferToObject(response.payload);
+    expect(Object.keys(payload).length).toBe(2);
+    expect(payload.a_aa_123aaad3.balance).toBe(200);
+    expect(payload.a_aa_123aaad3.name).toBe('KevinA');
+    expect(payload.a_aa_123aaad3.accountId).toBe('aa_123aaad3');
+    expect(payload.a_aa_dsf123ef3.balance).toBe(100);
+  });
+
+  test('fetchAllAccount', async () => {
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'aa_123aaad3', balance: 200, name: 'Kevin',
+    })]);
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'aa_dsf123ef3', balance: 100, name: 'Kevin',
+    })]);
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId: 'bb_dsf123ef3', balance: 500, name: 'Kevin',
+    })]);
+    const response = await mockStub.mockInvoke('tx1', ['fetchAllAccount', '{}']);
+
+    expect(response.status).toBe(200);
+    const payload = Transform.bufferToObject(response.payload);
+    expect(Object.keys(payload).length).toBe(3);
+    expect(payload.a_aa_123aaad3.balance).toBe(200);
+    expect(payload.a_aa_dsf123ef3.balance).toBe(100);
+    expect(payload.a_bb_dsf123ef3.balance).toBe(500);
+  });
+
+  test('deposit', async () => {
+    const accountId = '123aaad3dsfs';
+    const balance = 200;
+    const date1 = new Date().toUTCString();
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId, balance, name: 'Kevin',
+    })]);
+    DATE_TO_USE = sinon.useFakeTimers({ now: 1556948895861 }); // Set data to a new value
+    const date2 = new Date().toUTCString();
+
+    await mockStub.mockInvoke('tx1', ['deposit', JSON.stringify({
+      accountId, amount: 100,
+    })]);
+    await mockStub.mockInvoke('tx1', ['deposit', JSON.stringify({
+      accountId, amount: 50,
+    })]);
+
+    const response = await mockStub.mockInvoke('tx1', ['fetchTransactions', JSON.stringify({
+      accountId,
+    })]);
+
+    expect(response.status).toBe(200);
+    const payload = Transform.bufferToObject(response.payload);
+    expect(payload.length).toBe(5);
+    expect(payload[0].key).not.toBeUndefined();
+    expect(payload[0].key).not.toBeNull();
+    expect(payload[0].amount).toBe(200);
+    expect(payload[0].type).toBe(DEPOSIT_TYPE);
+    expect(payload[0].date).toBe(date1);
+
+    expect(payload[1].key).not.toBeUndefined();
+    expect(payload[1].key).not.toBeNull();
+    expect(payload[1].amount).toBe(4.96);
+    expect(payload[1].type).toBe(INTEREST_TYPE);
+    expect(payload[1].date).toBe(date2);
+
+    expect(payload[2].key).not.toBeUndefined();
+    expect(payload[2].key).not.toBeNull();
+    expect(payload[2].amount).toBe(100);
+    expect(payload[2].type).toBe(DEPOSIT_TYPE);
+    expect(payload[2].date).toBe(date2);
+
+    expect(payload[3].key).not.toBeUndefined();
+    expect(payload[3].key).not.toBeNull();
+    expect(payload[3].amount).toBe(0);
+    expect(payload[3].type).toBe(INTEREST_TYPE);
+    expect(payload[3].date).toBe(date2);
+
+    expect(payload[4].key).not.toBeUndefined();
+    expect(payload[4].key).not.toBeNull();
+    expect(payload[4].amount).toBe(50);
+    expect(payload[4].type).toBe(DEPOSIT_TYPE);
+    expect(payload[4].date).toBe(date2);
+
+    // Retrieve and compare the newest state
+    const newResponse = await mockStub.mockInvoke('tx1', ['queryAccount', JSON.stringify({ accountId: '123aaad3dsfs' })]);
+    expect(Transform.bufferToObject(newResponse.payload)).toEqual({
+      accountId: '123aaad3dsfs',
+      balance: 354.96,
+      name: 'Kevin',
+      lastUpdate: date2,
+    });
+  });
+
+  test('fetchTransactions', async () => {
+    const accountId = 'sdfsdfsd';
+    const balance = 200;
+    const date = new Date().toUTCString();
+
+    await mockStub.mockInvoke('tx1', ['createAccount', JSON.stringify({
+      accountId, balance, name: 'Kevin',
+    })]);
+
+    DATE_TO_USE = sinon.useFakeTimers({ now: 1556948895861 }); // Set data to a new value
+
+    await mockStub.mockInvoke('tx1', ['withdraw', JSON.stringify({
+      accountId, amount: 50,
+    })]);
+
+    const response = await mockStub.mockInvoke('tx1', ['fetchTransactions', JSON.stringify({
+      accountId,
+    })]);
+
+
+    expect(response.status).toBe(200);
+    const payload = Transform.bufferToObject(response.payload);
+    expect(payload.length).toBe(3);
+    expect(payload[0].key).not.toBeUndefined();
+    expect(payload[0].key).not.toBeNull();
+    expect(payload[0].amount).toBe(200);
+    expect(payload[0].type).toBe(DEPOSIT_TYPE);
+    expect(payload[0].date).toBe(date);
+
+    const newDate = new Date().toUTCString();
+    expect(payload[1].key).not.toBeUndefined();
+    expect(payload[1].key).not.toBeNull();
+    expect(payload[1].amount).toBe(4.96);
+    expect(payload[1].type).toBe(INTEREST_TYPE);
+    expect(payload[1].date).toBe(newDate);
+
+    expect(payload[2].key).not.toBeUndefined();
+    expect(payload[2].key).not.toBeNull();
+    expect(payload[2].type).toBe(WITHDRAW_TYPE);
+    expect(payload[2].amount).toBe(50);
+    expect(payload[2].date).toBe(newDate);
+
+    // Retrieve and compare the newest state
+    const newResponse = await mockStub.mockInvoke('tx1', ['queryAccount', JSON.stringify({ accountId: 'sdfsdfsd' })]);
+    expect(Transform.bufferToObject(newResponse.payload)).toEqual({
+      accountId: 'sdfsdfsd',
+      balance: 154.96,
+      name: 'Kevin',
+      lastUpdate: newDate,
+    });
+  });
+});
--- src/chaincode/Account.js
+++ src/chaincode/Account.js
@@ -6,17 +6,21 @@ const shim = require('fabric-shim');
 const ACCOUNT_PREFIX = 'a_';
 const TRANSACTION_PREFIX = 't_';
 const DEPOSIT_TYPE = 'DEPOSIT';
-const WITHDRAWAL_TYPE = 'WITHDRAWAL';
+const WITHDRAW_TYPE = 'WITHDRAW';
+const INTEREST_TYPE = 'INTEREST';
+
+const ANNUAL_INTEREST_RATE = 0.02;
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
 async function createTransaction(stub, {
   accountId, amount, type, date,
 }) { // appends a timestamp plus a 0 - 99 random number for the transaction key
-  return stub.putState(`${TRANSACTION_PREFIX}${accountId}_${Date.now()}${Math.random() * 100}`, Buffer.from(JSON.stringify({
+  return stub.putState(`${TRANSACTION_PREFIX}${accountId}_${Date.now()}${Math.floor(Math.random() * 100)}`, Buffer.from(JSON.stringify({
     type, date, amount,
   })));
 }
 
-async function parseResults(iterator) {
+async function parseAccounts(iterator) {
   const results = {};
   while (true) {
     const res = await iterator.next();
@@ -32,6 +36,36 @@ async function parseResults(iterator) {
       return Buffer.from(JSON.stringify(results));
     }
   }
+}
+
+async function parseTransations(iterator) {
+  const results = [];
+  while (true) {
+    const res = await iterator.next();
+    if (res.value && res.value.value.toString()) {
+      try {
+        results.push({ key: res.value.key, ...JSON.parse(res.value.value.toString('utf8')) });
+      } catch (err) {
+        results.push({ key: res.value.key, value: res.value.value.toString('utf8') });
+      }
+    }
+    if (res.done) {
+      await iterator.close();
+      return Buffer.from(JSON.stringify(results));
+    }
+  }
+}
+
+// a and b are javascript Date objects
+function dateDiffInDays(data1, data2) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(data1.getFullYear(), data1.getMonth(), data1.getDate());
+  const utc2 = Date.UTC(data2.getFullYear(), data2.getMonth(), data2.getDate());
+  return Math.floor((utc2 - utc1) / MS_PER_DAY);
+}
+
+function calculateInterest(currentBalance, startDate, endDate) {
+  return ((currentBalance * 100) * Math.floor(dateDiffInDays(startDate, endDate) / 365 * 100) * (ANNUAL_INTEREST_RATE * 100)) / 1000000;
 }
 
 const Account = class {
@@ -97,7 +131,7 @@ const Account = class {
 
     // Create a transaction for this account
     await createTransaction(stub, {
-      accountId: account.accountId, amount: account.amount, type: DEPOSIT_TYPE, date: account.date,
+      accountId: account.accountId, amount: account.balance, type: DEPOSIT_TYPE, date: account.lastUpdate,
     });
     // console.log('============= END : createAccount ===========');
   }
@@ -109,18 +143,69 @@ const Account = class {
   async queryAccounts(stub, args) {
     const { startAccountId, endAccountId } = JSON.parse(args);
     const iterator = await stub.getStateByRange(`${ACCOUNT_PREFIX}${startAccountId}`, `${ACCOUNT_PREFIX}${endAccountId}`);
-    return parseResults(iterator);
+    return parseAccounts(iterator);
   }
 
   async fetchAllAccount(stub) {
     const iterator = await stub.getStateByRange(`${ACCOUNT_PREFIX}0`, `${ACCOUNT_PREFIX}z`);
-    return parseResults(iterator);
+    return parseAccounts(iterator);
   }
 
   async fetchTransactions(stub, args) {
     const { accountId } = JSON.parse(args);
     const iterator = await stub.getStateByRange(`${TRANSACTION_PREFIX}${accountId}_0`, `${TRANSACTION_PREFIX}${accountId}_z`);
-    return parseResults(iterator);
+    return parseTransations(iterator);
+  }
+
+  async deposit(stub, args) {
+    const { accountId, amount } = JSON.parse(args);
+    const today = new Date();
+    const dateString = today.toUTCString();
+
+    const account = JSON.parse(await stub.getState(`${ACCOUNT_PREFIX}${accountId}`));
+
+    // Added interest first and than add deposit amount
+    const interest = calculateInterest(account.balance, new Date(account.lastUpdate), today);
+    account.balance = (account.balance * 100 + amount * 100 + interest * 100) / 100;
+
+    account.lastUpdate = dateString;
+
+    // update the account
+    await stub.putState(`${ACCOUNT_PREFIX}${account.accountId}`, Buffer.from(JSON.stringify(account)));
+
+    // Create a transaction for the interest
+    await createTransaction(stub, {
+      accountId, amount: interest, type: INTEREST_TYPE, date: dateString,
+    });
+    // Create a transaction for this deposit
+    await createTransaction(stub, {
+      accountId, amount, type: DEPOSIT_TYPE, date: dateString,
+    });
+  }
+
+  async withdraw(stub, args) {
+    const { accountId, amount } = JSON.parse(args);
+    const today = new Date();
+    const dateString = today.toUTCString();
+    const account = JSON.parse(await stub.getState(`${ACCOUNT_PREFIX}${accountId}`));
+
+    // Calculate the interest based on old amount and last update date
+    const interest = calculateInterest(account.balance, new Date(account.lastUpdate), today);
+    account.balance = (account.balance * 100 - amount * 100 + interest * 100) / 100;
+
+    account.lastUpdate = dateString;
+
+    // update the account
+    await stub.putState(`${ACCOUNT_PREFIX}${account.accountId}`, Buffer.from(JSON.stringify(account)));
+
+    // Create a transaction for the interest
+    await createTransaction(stub, {
+      accountId, amount: interest, type: INTEREST_TYPE, date: dateString,
+    });
+    // Create a transaction for this withdraw
+    await createTransaction(stub, {
+      accountId, amount, type: WITHDRAW_TYPE, date: dateString,
+    });
   }
 };
 
