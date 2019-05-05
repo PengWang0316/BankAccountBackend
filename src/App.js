6c970e48200e5e158eadd81a96f39825ff1ac280
Kevin Wang <rocwang0316@gmail.com>
2019-05-05 11:19:36 -0700

    Added the App.js file with a mock fetchAllAccount endpoint.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+
+
+app.get('/api/v1/fetchAllAccount', (req, res) => {
+  res.json({
+    '123adb': {
+      accountId: '123adb', balance: 300.23, name: 'Kevin Wang', lastUpdate: new Date('2018/10/01'),
+    },
+    aaad123: {
+      accountId: 'aaad123', balance: 3300.22, name: 'Joe W.', lastUpdate: new Date('2013/12/22'),
+    },
+  });
+});
+
+app.listen(8080, () => console.log('The web server listening on port 8080'));
