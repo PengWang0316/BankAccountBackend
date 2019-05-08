3ae70c933e64691baab5357bcab6deb72a2b8a3f
Kevin Wang <rocwang0316@gmail.com>
2019-05-08 14:33:18 -0700

    Updated some format.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const hfc = require('fabric-client');
 const WebSocketServer = require('ws');
 const log4js = require('log4js');
 
-// These four files below come from the AWS example
+// These four files below come from AWS example
 const query = require('./query.js');
 const connection = require('./connection.js');
 const blockListener = require('./blocklistener.js');
@@ -30,7 +30,6 @@ app.use(helmet());
 
 // Fabric config
 hfc.addConfigFile('config.json');
-// const host = 'localhost';
 const port = 3000;
 let username = '';
 let orgName = '';
@@ -47,7 +46,7 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     logger.info('##### Websocket Server received message: %s', message);
   });
-  ws.send('something');
+  ws.send('some messages');
 });
 
 
@@ -55,14 +54,6 @@ wss.on('connection', (ws) => {
 const BASE_VERSION = '/api/v1/';
 
 app.get(`${BASE_VERSION}accounts`, async (req, res) => {
-  // res.json({
-  //   '123adb': {
-  //     accountId: '123adb', balance: 300.23, name: 'Kevin Wang', lastUpdate: new Date('2018/10/01'),
-  //   },
-  //   aaad123: {
-  //     accountId: 'aaad123', balance: 3300.22, name: 'Joe W.', lastUpdate: new Date('2013/12/22'),
-  //   },
-  // });
   const message = await query.queryChaincode(peers, channelName, chaincodeName, {}, 'fetchAllAccount', username, orgName);
   res.json(message[0]);
 });
@@ -94,19 +85,11 @@ app.put(`${BASE_VERSION}account/withdraw`, async (req, res) => {
 // Register and enroll user. A user must be registered and enrolled before any queries
 // Use it to download the certification from CA
 // or transactions can be invoked
+// Comes from AWS example
 app.post('/users', async (req, res) => {
-  logger.info('================ POST on Users');
   ({ username, orgName } = req.body);
-  logger.info('##### End point : /users');
-  logger.info(`##### POST on Users- username : ${username}`);
-  logger.info(`##### POST on Users - userorg  : ${orgName}`);
   const response = await connection.getRegisteredUser(username, orgName, true);
-  logger.info('##### POST on Users - returned from registering the username %s for organization %s', username, orgName);
-  logger.info('##### POST on Users - getRegisteredUser response secret %s', response.secret);
-  logger.info('##### POST on Users - getRegisteredUser response secret %s', response.message);
   if (response && typeof response !== 'string') {
-    logger.info('##### POST on Users - Successfully registered the username %s for organization %s', username, orgName);
-    logger.info('##### POST on Users - getRegisteredUser response %s', response);
     // Now that we have a username & org, we can start the block listener
     await blockListener.startBlockListener(channelName, username, orgName, wss);
     res.json(response);
